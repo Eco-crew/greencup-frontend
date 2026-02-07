be5ebe05738d30e5c45e60fde4932473d197f421
namgijeong <dreaming1817@gmail.com>
2026-02-07 15:14:02 +0900

    [수거지점장-업체관리-업체상세] 휴무일 리스트 일부 ui

--- greencup-fe/src/components/reuse-operator/partner-manage/detail/holiday/HolidayList.jsx
+++ greencup-fe/src/components/reuse-operator/partner-manage/detail/holiday/HolidayList.jsx
@@ -1,11 +1,21 @@
 import './HolidayList.css';
 
+import Pagination from '../../../../pagination/Pagination';
 //수거지점장- 업체관리-상세페이지-휴무일 리스트
 export default function HolidayList(){
     return(
         <>
-        <div>
-            HolidayList
+        <div className="reuse-partner-manage-holiday-list-container">
+            <div className="reuse-partner-manage-holiday-title">휴무일</div>
+            <div className="reuse-partner-manage-holiday-list">
+                <div className="reuse-partner-manage-holiday-element">2026-02-06</div>
+                <div className="reuse-partner-manage-holiday-element">2026-02-10</div>
+                <div className="reuse-partner-manage-holiday-element">2026-02-14</div>
+                <div className="reuse-partner-manage-holiday-element">2026-02-14</div>
+                <div className="reuse-partner-manage-holiday-element">2026-02-14</div>
+                <div className="reuse-partner-manage-holiday-element">2026-02-14</div>
+                <div className="reuse-partner-manage-holiday-element">2026-02-14</div>
+            </div>
         </div>
         </>
     );
--- greencup-fe/src/pages/reuse-operator/partner-manage/ReuseOperatorPartnerManageDetailPage.jsx
+++ greencup-fe/src/pages/reuse-operator/partner-manage/ReuseOperatorPartnerManageDetailPage.jsx
@@ -1,13 +1,17 @@
-import './ReuseOperatorPartnerManageDetailPage.css';
+import "./ReuseOperatorPartnerManageDetailPage.css";
 
-import HolidayCalendar from '../../../components/reuse-operator/partner-manage/detail/holiday/HolidayCalendar';
+import HolidayCalendar from "../../../components/reuse-operator/partner-manage/detail/holiday/HolidayCalendar";
+import HolidayList from "../../../components/reuse-operator/partner-manage/detail/holiday/HolidayList";
 
-export default function ReuseOperatorPartnerManageDetailPage(){
-    return(
-        <>
-         <div className="reuse_partner_manage_detail_container">
-            <HolidayCalendar/>
-         </div>
-        </>
-    );
+export default function ReuseOperatorPartnerManageDetailPage() {
+  return (
+    <>
+      <div className="reuse_partner_manage_detail_container">
+        <div className="reuse_partner_manage_holiday-container">
+          <HolidayCalendar />
+          <HolidayList />
+        </div>
+      </div>
+    </>
+  );
 }

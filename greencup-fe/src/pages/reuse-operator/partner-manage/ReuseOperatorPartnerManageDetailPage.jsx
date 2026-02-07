0071be7bb8a54b32a07dc329a9f933169d79ca6e
namgijeong <dreaming1817@gmail.com>
2026-02-07 14:37:06 +0900

    [수거지점장-업체관리-업체상세] flatpickr 휴무일 읽기모드로 적용

--- greencup-fe/src/pages/reuse-operator/partner-manage/ReuseOperatorPartnerManageDetailPage.jsx
+++ greencup-fe/src/pages/reuse-operator/partner-manage/ReuseOperatorPartnerManageDetailPage.jsx
@@ -1,11 +1,13 @@
 import './ReuseOperatorPartnerManageDetailPage.css';
 
+import HolidayCalendar from '../../../components/reuse-operator/partner-manage/detail/holiday/HolidayCalendar';
+
 export default function ReuseOperatorPartnerManageDetailPage(){
     return(
         <>
-        <div>
-            ReuseOperatorPartnerManageDetailPage
-        </div>
+         <div className="reuse_partner_manage_detail_container">
+            <HolidayCalendar/>
+         </div>
         </>
     );
 }

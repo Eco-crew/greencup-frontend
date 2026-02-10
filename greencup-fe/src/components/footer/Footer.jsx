5ce63cd24da3be7156d78dcfdacd1a7743980941
injuyeon23 <injuyeon22@gmail.com>
2026-02-10 21:17:11 +0900

    [CSS] footer 제작

--- greencup-fe/src/components/footer/Footer.jsx
+++ greencup-fe/src/components/footer/Footer.jsx
@@ -1,9 +1,39 @@
 import "./Footer.css";
+
 export default function Footer() {
   return (
-    <>
-      {/* 재량껏 채워주세요 */}
-      <footer className="footer"></footer>
-    </>
+    // footer: 페이지 하단에 고정되는 공통 영역
+    <footer className="footer">
+      {/* footer 내부 콘텐츠 정렬을 위한 컨테이너 */}
+      <div className="footer-inner">
+        
+        {/* 상단 영역: 브랜드 + 크레딧 정보 */}
+        <div className="footer-top">
+          
+          {/* 브랜드 영역: 로고 + 서비스명 */}
+          <div className="footer-brand">
+            {/* public 폴더에 있는 로고 이미지 */}
+            <img
+              src="/img/greencup_login_logo.png"
+              alt="Greencup logo"
+              className="footer-logo"
+            />
+            {/* 서비스 이름 텍스트 */}
+            <span className="footer-title">Greencup</span>
+          </div>
+
+          {/* 크레딧 영역: 팀 정보 표시 */}
+          <div className="footer-credit">
+            <p>Eco Crew</p>
+            <p>남기정 · 김준성 · 김주연</p>
+          </div>
+        </div>
+
+        {/* 하단 영역: 저작권 문구 */}
+        <div className="footer-bottom">
+          <p>Copyright © Greencup. All rights reserved.</p>
+        </div>
+      </div>
+    </footer>
   );
 }

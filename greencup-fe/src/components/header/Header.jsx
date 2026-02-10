317a289058a117b33da30d4d03c8013dbe2cb9ec
injuyeon23 <injuyeon22@gmail.com>
2026-02-10 20:44:49 +0900

    [CSS] header 폰트, 글자 간격 수정

--- greencup-fe/src/components/header/Header.jsx
+++ greencup-fe/src/components/header/Header.jsx
@@ -3,6 +3,28 @@ import { NavLink, Route, Routes, useNavigate } from "react-router-dom";
 import "./Header.css";
 import { REUSE_OPERATOR, PARTNER } from "../../util/constant";
 
+/* 
+const bigMenu = {
+  noLogin: [
+    { label: "서비스 소개", path: "/" },
+    { label: "컵수거 시스템", path: "/system" },
+    { label: "공지사항", path: "/notice" },
+    { label: "소통 게시판", path: "/board" },
+  ],
+
+  reuseOperator: [
+    { label: "요청 현황", path: "/reuse-operator/requests" },
+    { label: "대여 현황", path: "/reuse-operator/partner-manage" },
+    { label: "수거 목록 통계", path: "/reuse-operator/stats" },
+  ],
+
+  partner: [
+    { label: "대여 관리", path: "/partner/request-settings" },
+    { label: "대여 기록", path: "/partner/requests" },
+    { label: "요청 및 반납 통계", path: "/partner/stats" },
+  ],
+};
+*/
 export default function Header({ loginUser, bigMenu, linkStyle }) {
   return (
     <>

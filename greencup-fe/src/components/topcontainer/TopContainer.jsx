bbc1456fce22e056fd9262836eb9f4189313d16c
injuyeon23 <injuyeon22@gmail.com>
2026-02-10 20:45:51 +0900

    [CSS] topcontainer 로고&아이콘 이미지 삽입, 폰트, 색 수정

--- greencup-fe/src/components/topcontainer/TopContainer.jsx
+++ greencup-fe/src/components/topcontainer/TopContainer.jsx
@@ -14,9 +14,11 @@ export default function TopContainer({ loginUser, logout }) {
   return (
     <>
       <div id="top_container">
-        <div id="logo_container">
-          <div>로고</div>
-          <div>GreenCup</div>
+        <div id="logo_container" onClick={goHomePage}>
+          <img 
+            src="/img/topcontainer/greencup_login_logo.png"
+            alt="GreenCup 로고" ></img>
+          <span className="logo_text">GreenCup</span>
         </div>
 
         <div id="login_info">
@@ -27,7 +29,11 @@ export default function TopContainer({ loginUser, logout }) {
           ) : (
             <>
               <div id="login_name">
-                <div>img</div>
+                <img
+                    src="/img/topcontainer/reuse-operator_icon.png"
+                    alt="수거지점장 아이콘"
+                    className="user_role_icon"
+                  />
                 <div>{loginUser.userName}님</div>
               </div>
               <div>고객지원</div>
